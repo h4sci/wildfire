141e1d52d954e430ac951b20cb93b281883e6cc4
schneiderst <mail@schneiderst.de>
2020-12-10 16:31:46 +0100

    Relative Path Structure (R-project)
    changed the path-setting in R after consulation with Matthias
    (required to create an r-project)

--- wild-fire-project2020.R
+++ wild-fire-project2020.R
@@ -3,8 +3,10 @@
 #Check2
 
 #Set working directory
-setwd("C:/Users/scstepha/Documents/Forschung/Wildfire") #enter working directory here (C:/Users/scstepha/Documents/wildfire)
-getwd()
+#setwd("C:/Users/scstepha/Documents/Forschung/Wildfire") #enter working directory here (C:/Users/scstepha/Documents/wildfire)
+    #by creating an R-project we can avoid setting the directory.
+getwd() 
+
 #Working directory Structure
   #Working Directory: Wildfire
     #Folder: Raw_Data [do not write in this folder when processing data]
@@ -44,7 +46,7 @@ urls<-Final_urls
 
 #Define URL folder where to save the data (destination)
 
-data.folder = "./Raw_Data/"
+data.folder = "Raw_Data/"
 #data.folder = "E:/Wild_fire_project/Unzip_file/"
 
 #Get file name from url, with file extention
@@ -75,7 +77,7 @@ tmpdir_R <- tempdir()
   #enter years and months here for which you want to load monthly shapefiles into R
   from_s <-2015
   to_s   <-2017
-  months_s<-c("6_","7_") #"1_","2_","3_","4_","5_","6_","7_","8_","9_","10_","11_","12_"
+  months_s<-c("6_") #"1_","2_","3_","4_","5_","6_","7_","8_","9_","10_","11_","12_"
   #The following lines define a string vector to load the sample
     sampleyears<-seq.int(from_s, to_s, 1)
     sampleyears <- as.character(sampleyears) 
@@ -90,7 +92,7 @@ tmpdir_R <- tempdir()
   
     for(z in loadsample){ #Loop to load shapefiles into R
       #Unzip downloaded data
-      (zipfile<-str_c(file.path("./Raw_Data//"),z,".zip"))
+      (zipfile<-str_c(file.path("Raw_Data/"),z,".zip"))
       unzip(zipfile, exdir = tmpdir_R)
       #Untar downloaded data (use/modify the next two lines if you want to untar to your disk)
       #untar(tarfile = file.path(tempd1, "/",z,".tar"), exdir = "./Raw_Data/Extracted/")
